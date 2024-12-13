6b54f70cae8d3a0bbaa88643abc5e044a0245073
Yash doe <yasgavande78@gmail.com>
2024-12-13 09:13:10 +0000

    update index.js to shorten urls

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import validUrl from 'valid-url';
+import { nanoid } from 'nanoid';
+
 const app = express();
+//middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const urlDatabase = {}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+//
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body
+
+  //validate url
+  if (!validUrl.isWebUri(url)) {
+    return res.json({ error: 'Invalid URL' })
+  }
+
+  //generate a short url identifier
+  const shorturl = nanoid(6)
+
+  //save the mapping in database
+  urlDatabase[shorturl] = url
+
+  res.json({
+    original_url: url,
+    short_url: shorturl,
+  })
+})
+
+//Api to redirect shorturl ot original url
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const { shortUrl } = req.params
+
+  const originalurl = urlDatabase[shortUrl]
+
+  if (!originalurl) {
+    return res.json({ error: 'No short URL found for the given input' })
+  }
+  res.redirect(originalurl)
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
